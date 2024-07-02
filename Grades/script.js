f85d403300b60f68d416fb57eac6514745184ee1
+ <+>
2024-07-02 15:13:52 +0300

    commit

--- Grades/script.js
+++ Grades/script.js
@@ -9,27 +9,21 @@ function Generator(){
         
     // }
     let result;
-    if (grade > 79 && grade <= 100) {
+    if (grade < 0 || grade > 100 || isNaN(grade)) {
+        alert("Invalid input")
+    } else if (grade > 79 && grade <= 100) {
         result = "A"
-        alert(`Student ${name}, of Admission ${adm}, your grade is ${result}`)
     } else if (grade < 79 && grade >= 60) {
         result = "B"
-        alert(`Student ${name}, of Admission ${adm}, your grade is ${result}`)
     } else if (grade > 49 && grade <= 59) {
         result = "C"
-        alert(`Student ${name}, of Admission ${adm}, your grade is ${result}`)
     } else if (grade > 40 && grade <= 49) {
         result = "D"
-        alert(`Student ${name}, of Admission ${adm}, your grade is ${result}`)
     } 
     else if(grade < 39){
         result = "E"
-        alert(`Student ${name}, of Admission ${adm}, your grade is ${result}`)
     }
-    else{
-        alert("Invalid input")
+    
 
-    }
-
-    // alert(`Student ${name}, o÷f Admission ${adm}, your grade is ${result}`);
+    alert(`Student ${name}, o÷f Admission ${adm}, your grade is ${result}`);
 }
